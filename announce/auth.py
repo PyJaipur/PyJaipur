edcbd900e3715e8a0a223c0e2cbd35a9a2002aee
arjoonn sharma <arjoonn.94@gmail.com>
2020-05-30 17:25:31 +0530

    try to move to oauthlib

--- announce/auth.py
+++ announce/auth.py
@@ -1,23 +1,27 @@
 import os
 import pickle
-from rauth import OAuth1Service
+import requests
+from requests_oauthlib import OAuth1Session, OAuth1
 
 
 def refresh_twitter(sessions):
     session = sessions.get("twitter")
+    request_token_url="https://api.twitter.com/oauth/request_token"
+    access_token_url="https://api.twitter.com/oauth/access_token"
+    authorize_url="https://api.twitter.com/oauth/authorize"
+    base_url="https://api.twitter.com/1.1/"
     try:
         params = {"include_rts": 1, "count": 10}
         r = session.get("statuses/home_timeline.json", params=params)
     except Exception:
-        twitter = OAuth1Service(
-            name="twitter",
-            consumer_key=os.environ.get("TWITTER_CONSUMER_KEY"),
-            consumer_secret=os.environ.get("TWITTER_CONSUMER_SECRET"),
-            request_token_url="https://api.twitter.com/oauth/request_token",
-            access_token_url="https://api.twitter.com/oauth/access_token",
-            authorize_url="https://api.twitter.com/oauth/authorize",
-            base_url="https://api.twitter.com/1.1/",
+        twitter = OAuth1Session(
+            client_key=os.environ.get("TWITTER_CONSUMER_KEY"),
+            client_secret=os.environ.get("TWITTER_CONSUMER_SECRET"),
         )
+        fetch_response = twitter.fetch_request_token(request_token_url)
+        {
+        oauth = OAuth1(client_key, client_secret=client_secret)
+
         request_token, request_token_secret = twitter.get_request_token()
         authorize_url = twitter.get_authorize_url(request_token)
         print(authorize_url)
