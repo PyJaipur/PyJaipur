f6df62d480cf4b303037e35291d4bba131f4d940
arjoonn sharma <arjoonn.94@gmail.com>
2020-09-26 11:23:28 +0530

    force render event

--- website/plugins.py
+++ website/plugins.py
@@ -1,8 +1,11 @@
 import pendulum
 
 
-def in_future(date):
+def in_future(date, force=None):
+    now = pendulum.now("Asia/Kolkata")
+    print(force, date, now)
+    if force is not None:
+        return force
     pre = date[:-9]
     start = pendulum.from_format(pre, "DD MMMM YYYY HH:mm:ss", tz="Asia/Kolkata")
-    now = pendulum.now("Asia/Kolkata")
     return start > now
