a89879d783e21861f8b7850dcdd65a8980e5a3ad
arjoonn sharma <arjoonn.94@gmail.com>
2020-09-26 11:14:41 +0530

    timezone in now calculation

--- website/plugins.py
+++ website/plugins.py
@@ -4,5 +4,5 @@ import pendulum
 def in_future(date):
     pre = date[:-9]
     start = pendulum.from_format(pre, "DD MMMM YYYY HH:mm:ss", tz="Asia/Kolkata")
-    now = pendulum.now()
+    now = pendulum.now("Asia/Kolkata")
     return start > now
